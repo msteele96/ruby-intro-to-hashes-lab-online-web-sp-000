f6340ede85b723d5f1610f174178c43c400ec61a
Marc Steele <marcsteele2014@gmail.com>
2020-10-11 22:48:36 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -18,14 +18,14 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-	monopoly = {:railroads => 
-		{pieces: 4, 
+	monopoly = {:railroads =>
+		{pieces: 4,
 		:names => {
 			 :reading_railroad => {},
 			 :pennsylvania_railroad => {},
 			 :b_and_o_railroad => {},
 			 :shortline => {}
-			 }, 
+			 },
 		:rent_in_dollars => {
 				one_piece_owned: 25,
 				two_pieces_owned: 50,
